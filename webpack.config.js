986b6f65091728d3e829a2cb918c5cf5e5a5ecea
bom\bomi <kk3p1q1@gmail.com>
2022-07-10 21:48:45 +0900

    실습2:웹팩 로더 실습

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,12 @@
-const path = require("path");
+const path = require('path');
 
-// es6모듈시스템이 아니라 노드의 모듈 시스템 사용하는것이라서 아래와 같은 키워드 사용
 module.exports = {
-  mode: "development",
+  mode: 'development',
   entry: {
-    main: "./src/app.js",
+    main: './src/app.js'
   },
   output: {
-    filename: "[name].js", // entry에서 일치하는 키값(main)으로 번들링파일 이름 정함. 동적으로 파일 생성위해
-    path: path.resolve("./dist"),
-  },
-};
+    filename: '[name].js',
+    path: path.resolve('./dist'),
+  }
+}
