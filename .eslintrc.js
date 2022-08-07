72b4f3df47d88bcd7396f7b3c89ada2485e6ffb7
bom\bomi <kk3p1q1@gmail.com>
2022-08-07 23:42:15 +0900

    실습:3-1린트 - 해결

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,18 @@
-// TODO: eslint를 구성해 보세요. 
+// TODO: eslint를 구성해 보세요.
+/*
+    1. npm i eslint
+    2. npx eslint --init 한 후 터미널에서 설정을 선택해줌
+    3. 린트설정파일에 설정이 생성됨
+*/
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {},
+};
