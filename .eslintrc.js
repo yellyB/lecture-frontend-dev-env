930e63292466880cb29bc2328178472636740a96
bom\bomi <kk3p1q1@gmail.com>
2022-08-08 21:30:32 +0900

    실습:3-2 프리티어 - 해결

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,26 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    // TODO: 프리티어 설정을 추가하세요.
-    "rules": {
-    }
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  // TODO: 프리티어 설정을 추가하세요.
+  /*
+    1. app.js에서 한것처럼 수동으로 돌리는건 귀찮음.
+        때문에 자동으로 코드 수정되도록 설정하기
+
+        npm i eslint-config-pretter eslint-plugin-prettier 로 두가지 설치
+
+    2. rules에 추가해도 되지만 간단하게 extends에서 prettier plugin을 설정해줌
+    3. npm run lint로 이미 추가되어있는 lint 스크립트를 돌려줌
+    */
+  rules: {},
 };
--- src/app.js
+++ src/app.js
@@ -2,12 +2,16 @@
  * TODO: Prettier가 스타일을 수정합니다.
  */
 
+/*
+    1. npm i prettier 로 프리티어 설치
+    2. npx prettier src/app.js --write 로 코드 정리
+      싱글쿼터가 더블쿼트로, 생략된 세미콜론이 추가됨, 애로우펑션 빈공간, 인덴트 추가
+*/
 
-import MainController from './controllers/MainController.js'
+import MainController from "./controllers/MainController.js";
 
-import './app.scss'
+import "./app.scss";
 
-
-document.addEventListener("DOMContentLoaded", ()=>{
-new MainController(); 
-})
+document.addEventListener("DOMContentLoaded", () => {
+  new MainController();
+});
