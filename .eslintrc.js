8e6c2adb4b939c4830b1de4c8b119a3117aa21a1
bom\bomi <kk3p1q1@gmail.com>
2022-08-08 21:12:59 +0900

    실습:3-2 린트-프리티어

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
-// TODO: eslint를 구성해 보세요.
-/*
-    1. npm i eslint
-    2. npx eslint --init 한 후 터미널에서 설정을 선택해줌
-    3. 린트설정파일에 설정이 생성됨
-*/
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: "eslint:recommended",
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  rules: {},
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    // TODO: 프리티어 설정을 추가하세요.
+    "rules": {
+    }
 };
--- src/app.js
+++ src/app.js
@@ -1,21 +1,13 @@
 /**
- * TODO eslint가 중복 세미콜론을 제거합니다.
+ * TODO: Prettier가 스타일을 수정합니다.
  */
 
-/*
-    1. package.json에 lint스크립트 추가해줌.
-        --fix옵션으로 자동으로 소스 고쳐지도록(문제에 나온 중복 세미콜론 제거됨)
-    2. 그리고 터미널에 
-         10:7  error  'foo' is assigned a value but never used  no-unused-vars
-        이와 같은 에러를 볼 수 있는데 foo 변수가 사용되지 않는다는 경고임
-*/
 
-import MainController from "./controllers/MainController.js";
+import MainController from './controllers/MainController.js'
 
-import "./app.scss";
+import './app.scss'
 
-const foo = "";
 
-document.addEventListener("DOMContentLoaded", () => {
-  new MainController();
-});
+document.addEventListener("DOMContentLoaded", ()=>{
+new MainController(); 
+})
